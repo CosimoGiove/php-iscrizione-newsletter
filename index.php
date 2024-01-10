2cb1ba355084e1a375f7582f305353dbafb6878e
cosimogiove <cosimo18497@gmail.com>
2024-01-10 15:32:53 +0100

    feat: include

--- index.php
+++ index.php
@@ -16,26 +16,7 @@
             <input type="submit">
         </form>
         <?php
-        function validateEmail($email)
-        {
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        if (isset($_GET['email'])) {
-            $email = $_GET['email'];
-            if (validateEmail($email)) {
-                echo "<div class='alert alert-success'>l'indirizzo email contiene un punto e una chiocciola $email</div>";
-              
-            } else {
-                echo "<div class='alert alert-warning'>
-                 errore, l'email non contiene un punto è una chiocciola $email
-              </div>";
-            }
-        }
+        include __DIR__."/functions.php"
         ?>
     </main>
 </body>
